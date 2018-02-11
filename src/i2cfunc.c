f7ace9e5532f88f41823327e6b81039ef0df2cf8
Scott Fasone <scott181182@yahoo.com>
2018-02-11 10:44:08 -0500

    No longer exits process on error

--- src/i2cfunc.c
+++ src/i2cfunc.c
@@ -5,7 +5,7 @@
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
-#include <errno.h>
+
 #include "i2cfunc.h"
 
 
@@ -13,33 +13,14 @@
 char* i2c_file = "/dev/i2c-2";
 int curr_reg;
 
-
-int i2c_init()
-{
-    int fd = open(i2c_file, O_RDWR);
-    if(fd < 0)
-    {
-        perror("i2c_init error:");
-        exit(1);
-    }
-    return fd;
-}
-int i2c_init_file(char* filename)
-{
-    int fd = open(filename, O_RDWR);
-    if(fd < 0)
-    {
-        perror("i2c_init error:");
-        exit(1);
-    }
-    return fd;
-}
+int i2c_init_file(char* filename) { return open(filename, O_RDWR); }
+int i2c_init() { return i2c_init_file(i2c_file); }
 
 void _i2c_set_addr(int adapter, int addr)
 {
     if(addr != curr_reg) {
         if(ioctl(adapter, I2C_SLAVE, addr) < 0) {
-            perror("_i2c_change_reg error:");
+            //perror("_i2c_change_reg error:");
             curr_reg = -1;
         } else {
             curr_reg = addr;
@@ -55,7 +36,7 @@ tI2C_Status i2c_write(int adapter, int address, unsigned char* buffer, unsigned 
 
     if(write(adapter, buffer, length) != length)
     {
-        perror("i2c_write error:");
+        //perror("i2c_write error:");
         return -1;
     }
     return length;
@@ -67,7 +48,7 @@ tI2C_Status i2c_read(int adapter, int address, unsigned char* buffer, unsigned i
 
     if(read(adapter, buffer, length) != length)
     {
-        perror("i2c_read error:");
+        //perror("i2c_read error:");
         return -1;
     }
     return length;
@@ -79,7 +60,7 @@ int i2c_close(int adapter)
 {
     if ((close(adapter)) != 0)
     {
-        perror("i2c_close error:");
+        //perror("i2c_close error:");
         return -1;
     }
     return 0;
